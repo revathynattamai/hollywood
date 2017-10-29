501d97358009dde85fde79c1061e0489191de052
revathynattamai <revathy.ng@gmail.com>
2017-10-30 00:21:51 +0530

    Added webpack,babel,server files

---
+++ src/components/web/app/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+export class Hello extends Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Hello />,
+    document.getElementById('root')
+);
---
+++ webpack/webpack.config.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, '../dist/');
+var APP_DIR = path.resolve(__dirname, '../src');
+
+console.log(BUILD_DIR);
+console.log(APP_DIR);
+var config = {
+  entry: APP_DIR + '/components/web/app/app.js',
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js'
+  },
+  module : {
+    loaders : [
+      {
+        test : /\.jsx?/,
+        include : APP_DIR,
+        loader : 'babel-loader'
+      }
+    ]
+  }
+};
+
+module.exports = config;
