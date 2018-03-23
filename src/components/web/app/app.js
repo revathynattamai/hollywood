88e5591e241226003d7bef9524046d90030db730
revathynattamai <revathy.ng@gmail.com>
2018-03-23 10:15:05 +0530

    Added input number component

--- src/components/web/app/app.js
+++ src/components/web/app/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Heading from './heading'; 
-
+import InputNum from './inputNum';
 export class Hello extends Component {
     render() {
         return (
+            <div>
             <Heading />
+            <InputNum />
+            </div>
         );
     }
 }
