faf11486afc7c477851a155cba7553b942b3689a
revathynattamai <revathy.ng@gmail.com>
2018-03-23 10:37:18 +0530

    Adding redux

--- src/components/web/app/app.js
+++ src/components/web/app/app.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
 import ReactDOM from 'react-dom';
+
 import Heading from './heading'; 
-import InputNum from './inputNum';
+import InputNum from './inputNum'; 
+import TeBo from './tebo'
+
+
+
 export class Hello extends Component {
     render() {
         return (
             <div>
             <Heading />
             <InputNum />
+            <TeBo />
             </div>
         );
     }
